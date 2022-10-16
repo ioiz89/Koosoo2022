41b1a08e3958e096c0c15bb58f25e13c43f12c88
redbean <redbean@vivestudios.com>
2022-10-16 16:56:56 +0900

    선넘지마 에서 사용할 스태틱 값 지정

---
+++ Assets/Scripts/Game/DCTL_VALUES.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DCTL
+{
+    public class PoleLines
+    {
+        public static int MainLine = 0;
+        public static int SubLine = 1;
+    }
+
+    public enum ELineDir
+    {
+        r, t, l, b, none
+    }
+}
