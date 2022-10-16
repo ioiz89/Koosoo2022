e9b226ff80e9d9ca050c4ebefda49e587d2f9b39
redbean <redbean@vivestudios.com>
2022-10-16 18:14:36 +0900

    차단봉의 이름에 맞추어 줄의 방향을 변경하는 코드 작성
    close #1

--- Assets/Scripts/Prop/PoleDirectionSetter.cs
+++ Assets/Scripts/Prop/PoleDirectionSetter.cs
@@ -9,16 +9,30 @@ public class PoleDirectionSetter : MonoBehaviour
     public List<Transform> lines;
     public List<ELineDir> dir = new List<ELineDir>();
 
-
     // Start is called before the first frame update
     void Start()
     {
-        //lines = new List<Transform>();
-        //lines.Add(this.transform.GetChild(PoleLines.MainLine));
-        //lines.Add(this.transform.GetChild(PoleLines.SubLine));
-
+        dir.Capacity = 2;
+        SetLinesDir();
         ChangeDir(PoleLines.MainLine, dir[PoleLines.MainLine]);
-        ChangeDir(PoleLines.SubLine, dir[PoleLines.SubLine]);
+        ChangeDir(PoleLines.SubLine,  dir[PoleLines.SubLine]);
+    }
+
+    
+    private void SetLinesDir()
+    {
+        var name = this.gameObject.name;
+        var verticalLineNum = name.Split('_')[1];
+        var horizonLineNum = name.Split('_')[2];
+        int verTemp = 0;
+        int horizonTemp = 0;
+        if(int.TryParse(verticalLineNum, out verTemp) && int.TryParse(horizonLineNum, out horizonTemp))
+        {
+            verTemp = Mathf.Abs(verTemp - 7);
+            
+            dir[PoleLines.MainLine] = PoleDirectionInstance.GetDir(verTemp, horizonTemp, PoleLines.MainLine);
+            dir[PoleLines.SubLine]  = PoleDirectionInstance.GetDir(verTemp, horizonTemp, PoleLines.SubLine);
+        }
     }
 
     private void ChangeDir(int lineID, ELineDir i)
@@ -29,7 +43,7 @@ public class PoleDirectionSetter : MonoBehaviour
             return;
         }
         Vector3 rot = new Vector3();
-        rot.y = rot.y + (90.0f * (int)i);
+        rot.y = rot.y + (float)i;
         lines[lineID].rotation = Quaternion.Euler(rot);
     }
 }
